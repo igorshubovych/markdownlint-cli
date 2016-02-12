fcb19c6b2064332ad8f0007d129f54d9366508a5
Igor Shubovych <igor.shubovych@gmail.com>
2016-02-12 14:17:24 +0200

    More tests

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 import test from 'ava';
 import execa from 'execa';
 
-test(async t => {
-  t.true((await execa('./markdownlint.js', ['--version'])).stdout.length > 0);
+test('--version option', async t => {
+  const result = await execa('./markdownlint.js', ['--version']);
+  t.true(result.stdout.length > 0);
+});
+
+test('--help option', async t => {
+  const result = await execa('./markdownlint.js', ['--help']);
+  t.true(result.stdout.indexOf('markdownlint') >= 0);
+  t.true(result.stdout.indexOf('--version') >= 0);
+  t.true(result.stdout.indexOf('--help') >= 0);
 });
