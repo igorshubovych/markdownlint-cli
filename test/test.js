d1e048d25e7de7da74de03888ad547dac5a22c51
Igor Shubovych <igor.shubovych@gmail.com>
2017-04-13 23:07:32 +0300

    Fix linting

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ import test from 'ava';
 import execa from 'execa';
 
 test('--version option', async t => {
-  const result = execa.sync('../markdownlint.js', ['--version']);
+  const result = await execa('../markdownlint.js', ['--version']);
   t.true(result.stdout.length > 0);
 });
 
 test('--help option', async t => {
-  const result = execa.sync('../markdownlint.js', ['--help']);
+  const result = await execa('../markdownlint.js', ['--help']);
   t.true(result.stdout.indexOf('markdownlint') >= 0);
   t.true(result.stdout.indexOf('--version') >= 0);
   t.true(result.stdout.indexOf('--help') >= 0);
@@ -17,7 +17,7 @@ test('--help option', async t => {
 
 test('linting of correct Markdown file returns no error', async t => {
   try {
-    const result = execa.sync('../markdownlint.js',
+    const result = await execa('../markdownlint.js',
       ['--config', 'test-config.json', 'correct.md']);
     t.true(result.stderr.length === 0);
   } catch (err) {
@@ -27,10 +27,9 @@ test('linting of correct Markdown file returns no error', async t => {
 
 test('linting of incorrect Markdown file fails', async t => {
   try {
-    const result = execa.sync('../markdownlint.js',
+    await execa('../markdownlint.js',
       ['--config', 'test-config.json', 'incorrect.md']);
-    t.true(result.stderr.length > 0);
   } catch (err) {
-    console.log(err);
+    t.true(err.stderr.length > 0);
   }
 });
