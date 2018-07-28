48ab1987c77efab9a4844b672455ad0ff37dce58
Aleksandr Veselov <veselov143@gmail.com>
2018-07-28 18:38:25 +0300

    Fix bad executable file resolution for Windows in tests

--- test/test.js
+++ test/test.js
@@ -395,9 +395,10 @@ test('Custom rule from node_modules package loaded', async t => {
 });
 
 test('Custom rule from node_modules package loaded relative to cwd', async t => {
+  console.log(path.join(__dirname, 'custom-rules', 'relative-to-cwd'));
   try {
     var input = '# Input';
-    await execa('../../../markdownlint.js',
+    await execa(path.resolve('..', 'markdownlint.js'),
       ['--rules', 'test-rule-package', '--stdin'], {
         input,
         cwd: path.join(__dirname, 'custom-rules', 'relative-to-cwd')
